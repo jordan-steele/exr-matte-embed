93ca6ec6424aa4f655905680aa769443db530113
Jordan Steele <jordan@jordan-steele.com>
2025-01-08 11:36:36 -0800

    Fixed Icon for Windows UI

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-import sys
+import sys, os
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
 from src.gui.main_window import EXRProcessorGUI
 from src.processing.exr_processor import EXRProcessor
 from version import get_version
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 def main():
     app = QApplication(sys.argv)
     processor = EXRProcessor()
     window = EXRProcessorGUI(processor)
     window.setWindowTitle(f"EXR Matte Embed v{get_version()}")
+    window.setWindowIcon(QIcon(resource_path("images/icon.ico")))
     window.show()
     sys.exit(app.exec())
 
